76d237d856ff8a9d7ea5cbd95d5fb38504090c7c
Marcela <marcelacsr@id.uff.br>
2020-02-06 11:47:12 -0300

    compreendendo paths

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,15 @@ class ArticlesController < ApplicationController
 
   end
 
+  # articles#create - articles_path - /articles
   def create
     # byebug
     render plain: params[:article].inspect
   end
 
 end
+
+
+# articles_path
+# /articles	  articles#index MOSTRAR TODOS OS ARTICLES
+# /articles   articles#create    form { form.submit }   CRIAR, SALVAR NO BANCO
