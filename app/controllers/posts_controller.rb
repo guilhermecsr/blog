8a549a8618e43a0046b1b5d70486a70b4e52e8f8
Marcela <marcelacsr@id.uff.br>
2020-02-10 18:59:00 -0300

    mais um dia de aprendizado

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,20 +7,23 @@ class PostsController < ApplicationController
   # posts#create - posts_path - /posts
   def create
     # byebug
-    render plain: params[:post].inspect
-    # @post = Post.new(posts_params)
-    # if @post.save
+    # render plain: params[:post].inspect
+    @post = Post.new(posts_params)
+    # byebug
+    if @post.save
     #   mandar uma msg pro use5r dizenmdo que salvou
-    #   redirect_to post_path(@post)
-    # else
+      redirect_to post_path(@post)
+    else
+      redirect_to new_post_path
     #   mandar uma msg pro user dixzendi q n salvou
-    # end
+    end
   end
 
 
   def show
-
+    @post = Post.find(params[:id])
   end
+  #
 
   private
 
