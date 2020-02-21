1a566afc2914d3e16d3873a76b5caf0570afd5b7
Marcela <marcelacsr@id.uff.br>
2020-02-21 19:15:12 -0300

    adicionado: destroy dos posts, controller, routes, model e migration de comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
   # localhost:3000
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   root 'home#index'
 end
